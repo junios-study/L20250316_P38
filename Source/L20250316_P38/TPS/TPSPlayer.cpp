c77225bf92fd6f8f7409218bf52d6f43cd5a8c03
kadco <kadco69@naver.com>
2026-03-30 14:53:16 +0900

    TPS 추가

---
+++ Source/L20250316_P38/TPS/TPSPlayer.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSPlayer.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+ATPSPlayer::ATPSPlayer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+
+	FollorCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollorCamera"));
+	FollorCamera->SetupAttachment(CameraBoom);
+
+	GetMesh()->SetRelativeLocationAndRotation(
+		FVector(0, 0, -GetCapsuleComponent()->GetScaledCapsuleHalfHeight()),
+		FRotator(0, -90.0f, 0)
+		);
+
+}
+
+// Called when the game starts or when spawned
+void ATPSPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATPSPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATPSPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
