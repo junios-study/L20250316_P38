d96f1ea46fdc68723a4f62fa9f1eb3d93f6b62d5
kadco <kadco69@naver.com>
2026-04-13 14:50:10 +0900

    AI

---
+++ Source/L20250316_P38/TPS/ZombieAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ZombieAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class L20250316_P38_API UZombieAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
