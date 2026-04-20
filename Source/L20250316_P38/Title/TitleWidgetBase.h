5ddd50422759a3aacef430bb9bfbfa24aec3b88d
kadco <kadco69@naver.com>
2026-04-20 14:53:29 +0900

    멀티플레이 설정

---
+++ Source/L20250316_P38/Title/TitleWidgetBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TitleWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class L20250316_P38_API UTitleWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
