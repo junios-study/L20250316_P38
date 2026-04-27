7c91dbf2f8e4fa7547da01c1996cde22090d731f
kadco <kadco69@naver.com>
2026-04-27 10:27:56 +0900

    로비 위젯 작업

---
+++ Source/L20250316_P38/Lobby/LobbyWidgetBase.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyWidgetBase.h"
+
+void ULobbyWidgetBase::NativeOnInitialized()
+{
+
+}
+
+void ULobbyWidgetBase::ProcessCommited(const FText& Text, ETextCommit::Type CommitMethod)
+{
+}
+
+void ULobbyWidgetBase::ProcessStart()
+{
+}
+
+void ULobbyWidgetBase::ProceeSend()
+{
+}
+
+
+void ULobbyWidgetBase::UpdateLeftTime(int32 InLeftTime)
+{
+
+}
+
+void ULobbyWidgetBase::UpdateConnectionCount(int32 InConnectionCount)
+{
+
+}
---
+++ Source/L20250316_P38/Lobby/LobbyWidgetBase.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LobbyWidgetBase.generated.h"
+
+class UScrollBox;
+class UEditableTextBox;
+class UTextBlock;
+class UButton;
+
+/**
+ * 
+ */
+UCLASS()
+class L20250316_P38_API ULobbyWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+
+	virtual void NativeOnInitialized() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UScrollBox> ChatScrollBox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> Inputbox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UTextBlock> LeftTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ConnectionCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UButton> SendButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UButton> StartButton;
+
+	UFUNCTION(BlueprintCallable)
+	void ProcessCommited(const FText& Text, ETextCommit::Type CommitMethod);
+
+	UFUNCTION(BlueprintCallable)
+	void ProcessStart();
+
+	UFUNCTION(BlueprintCallable)
+	void ProceeSend();
+
+	UFUNCTION()
+	void Start();
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateLeftTime(int32 InLeftTime);
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateConnectionCount(int32 InConnectionCount);
+}
+
